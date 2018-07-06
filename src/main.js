120c38a3c16b62494d81c501e2d8fd53e7b54d45
Matt Duffield <matt.duffield@gmail.com>
2018-07-06 15:43:20 -0700

    Auto commit from FrontEnd Creator

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import {PLATFORM} from 'aurelia-pal';
 import 'babel-polyfill';
 import * as Bluebird from 'bluebird';
+import 'jquery';
+import 'kendo.all.min';
 
 export async function configure(aurelia) {
   aurelia.use
     .standardConfiguration()
-    .developmentLogging();
+    .developmentLogging()
+    .plugin('aurelia-kendoui-bridge');
+
 
   await aurelia.start();
   await aurelia.setRoot(PLATFORM.moduleName('app'));
