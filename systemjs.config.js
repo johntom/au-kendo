93610d86d9a4a8aa166fade4a7a2e67e49519901
Matt Duffield <matt.duffield@gmail.com>
2018-07-06 17:34:58 -0700

    Auto commit from FrontEnd Creator

--- systemjs.config.js
+++ systemjs.config.js
@@ -16,7 +16,6 @@
     },
     paths: {
       'npm:': 'https://unpkg.com/'
-      // 'aurelia-kendoui-bridge': 'https://unpkg.com/aurelia-kendoui-bridge@1.7.0/dist/amd/'
     },
     map: {
       'plugin-babel': 'npm:systemjs-plugin-babel@0.0.25/plugin-babel.js',
@@ -25,8 +24,7 @@
       'plugin-json': 'npm:systemjs-plugin-json@0.3.0/json.js',
       'plugin-text': 'npm:systemjs-plugin-text@0.0.11/text.js',
       'babel-polyfill': 'npm:babel-polyfill@^6.26.0/dist/polyfill.min.js',
-      'bluebird': 'npm:bluebird@3.5.1/js/browser/bluebird.min.js',
-      'aurelia-kendoui-bridge': 'npm:aurelia-kendoui-bridge@1.7.0/dist/amd/'
+      'bluebird': 'npm:bluebird@3.5.1/js/browser/bluebird.min.js'
     },
     packages: {
       '/': {
