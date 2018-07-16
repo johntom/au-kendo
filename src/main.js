3857a45b22a5a17fdb01b0c8cdd611ced8debcd8
Matt Duffield <matt.duffield@gmail.com>
2018-07-16 16:39:15 -0700

    Auto commit from FrontEnd Creator

--- src/main.js
+++ src/main.js
@@ -6,7 +6,8 @@ export async function configure(aurelia) {
   aurelia.use
     .standardConfiguration()
     .developmentLogging()
-    .plugin('aurelia-kendoui-bridge');
+    .plugin('aurelia-kendoui-bridge')
+    .plugin('aurelia-bootstrap-select');
     // .plugin('aurelia-kendoui-bridge', (kendo) => kendo.pro());
 
   await aurelia.start();
---
+++ src/views/ab-select/ab-select.js
@@ -0,0 +1,18 @@
+export class AbSelect {
+
+  constructor() {
+
+  }
+}
+
+export class AbSelect {
+  
+  myCollection = [
+    { id: 1, option: 'Ketchup', group: 'Condiments' },
+    { id: 2, option: 'Mustard', group: 'Condiments' },
+    { id: 10, option: 'Steam', group: 'Breads' },
+    { id: 12, option: 'Toasted', group: 'Breads' },
+  ];
+
+  
+}
