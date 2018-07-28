d555076f2e08f8d660c175df09b64f081862ba65
Matt Duffield <matt.duffield@gmail.com>
2018-07-27 20:49:23 -0700

    Auto commit from FrontEnd Creator

--- src/welcome.js
+++ src/welcome.js
@@ -1,16 +1,17 @@
-//import {computedFrom} from 'aurelia-framework';
+// import {computedFrom} from 'aurelia-framework';
+import moment from 'moment';
 
 export class Welcome {
-  heading = 'Welcome to the Aurelia Navigation App!';
+  heading = 'Welcome to the Aurelia Navigation App! v1';
   firstName = 'John';
   lastName = 'Doe';
   previousValue = this.fullName;
 
-  //Getters can't be directly observed, so they must be dirty checked.
-  //However, if you tell Aurelia the dependencies, it no longer needs to dirty check the property.
-  //To optimize by declaring the properties that this getter is computed from, uncomment the line below
-  //as well as the corresponding import above.
-  //@computedFrom('firstName', 'lastName')
+  // Getters can't be directly observed, so they must be dirty checked.
+  // However, if you tell Aurelia the dependencies, it no longer needs to dirty check the property.
+  // To optimize by declaring the properties that this getter is computed from, uncomment the line below
+  // as well as the corresponding import above.
+  // @computedFrom('firstName', 'lastName')
   get fullName() {
     return `${this.firstName} ${this.lastName}`;
   }
